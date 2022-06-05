353d292320975effea312893f094f476f30b0c64
sammykingx <hellosammy@sammykingx.com.ng>
2022-06-05 14:21:39 -0700

    Task 6 c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: sammy
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sammy;
+
+	for (sammy = 48; sammy < 58; sammy++)
+		putchar(sammy);
+	putchar('\n');
+	return (0);
+}
