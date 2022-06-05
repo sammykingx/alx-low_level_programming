a95f97cf21080761f258b08a8b9dd5448b71dcb4
sammykingx <hellosammy@sammykingx.com.ng>
2022-06-05 14:06:08 -0700

    c program to print single digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: sammy
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int sammy;
+
+	for (sammy = 0; sammy < 10; sammy++)
+	{
+		printf("%d", sammy);
+	}
+	printf("\n");
+	return (0);
+}
