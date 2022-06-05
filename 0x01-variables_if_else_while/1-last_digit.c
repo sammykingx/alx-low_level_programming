7dbbef0a226d435303c87fd3d913c1f39b245a82
sammykingx <hellosammy@sammykingx.com.ng>
2022-06-05 13:13:37 -0700

    c program to print the less digits of random numbers

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * author: Sammykingx
+ * date:05-06-2022
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n % 10 > 5)
+		printf("Last digit 0f %d is %d is and greater than 5\n", n, n % 10);
+	else if (n % 10 == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	else if ((n % 10 < 6) && n % 10 != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	return (0);
+}
