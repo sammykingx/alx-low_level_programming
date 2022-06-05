b6e6b6c481a5807a81a5f8207dd3726f7bebd816
sammykingx <hellosammy@sammykingx.com.ng>
2022-06-05 12:22:07 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main -Entry point
+ * Author: Sammykingx
+ * Date: 05-06-2022
+ * Details: c program to print -ve, zero and -ve results of a random number
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,9 @@
+# About Folder
+This folder marks the beginning of my journey in learning and testing out my knowledge of variables, loops, if statements, else if staements etc.
+
+## Contents
+* Variables (int, char)
+* if statements
+* if-else statements
+* else-if statemnts
+* loops (for, while, do-while)
