afd5d8570fea43b4dd7ddf6a11ac36717973d9e9
sammykingx <hellosammy@sammykingx.com.ng>
2022-06-16 11:38:09 -0700

    using puts to print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 /**
- * * main - Entry point
+ * main - Entry point
+ *
+ * Author: Sammykingx
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	char string[70] = "\"Programming is like building a multilingual puzzle\"";
+	
+	puts(string);
+
+	/* Tried printing it directly */
+
+	puts("\"Programming is like building a multilingual puzzle\"");
 	return (0);
 }
