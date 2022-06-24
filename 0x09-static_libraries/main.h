03d32e2cf49848bf7cdf578f3fb982a219953fd8
sammykingx <hellosammy@sammykingx.com.ng>
2022-06-24 07:37:58 -0700

    Created the static library using the manua

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,70 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
-
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
 int _isupper(int c);
-
 int _isdigit(int c);
-
-int mul(int a, int b);
-
-void print_numbers(void);
-
-void print_most_numbers(void);
-
-void more_numbers(void);
-
-void print_line(int n);
-
-void print_diagonal(int n);
-
-void print_square(int size);
-
-void print_triangle(int size);
-
-void print_number(int n);
-
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 char *_strcat(char *dest, char *src);
-
 char *_strncat(char *dest, char *src, int n);
-
 char *_strncpy(char *dest, char *src, int n);
-
 int _strcmp(char *s1, char *s2);
-
-void reverse_array(int *a, int n);
-
-char *string_toupper(char *);
-
-char *cap_string(char *);
-
-char *leet(char *);
-
-char *rot13(char *);
-
-void print_number(int n);
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
-void print_buffer(char *b, int size);
-
 char *_memset(char *s, char b, unsigned int n);
-
 char *_memcpy(char *dest, char *src, unsigned int n);
-
 char *_strchr(char *s, char c);
-
 unsigned int _strspn(char *s, char *accept);
-
 char *_strpbrk(char *s, char *accept);
-
 char *_strstr(char *haystack, char *needle);
-
-void print_chessboard(char (*a)[8]);
-
-void print_diagsums(int *a, int size);
-
-void set_string(char **s, char *to);
-
 #endif
---
+++ 0x09-static_libraries/test.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(void)
+{
+	_puts("\"At the endnof the day, my goal was to be the best hacker\"\n\t- Kevin mitnick");
+
+	return (0);
+}
