2b5410fe24a5ba5d6755e003cfe0e6c12011a7f3
sammykingx <hellosammy@sammykingx.com.ng>
2022-06-28 11:38:47 -0700

    function that creates an array of chars on the heap

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * crete_array - creates an array of chars dynamically
+ * @size: size of the array
+ * @c: the string
+ * Author: Sammykingx
+ * Return: 0 means success except defined otherwise
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if ( size == 0)
+		return (NULL);
+
+	else if
+		char *memloc = (char *)malloc(size * sizeof(char));
+
+	if (memloc == NULL);
+		return (NULL);
+
+	return (memloc);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+# About folder 
+
+learning c dynamic memory allocation using the idea of pointers. malloc(), free(). This is part of the curricullum of the ALX SE Program
