a1b5aea45de6257ca44716853a6f6d6d230ecbf3
sammykingx <hellosammy@sammykingx.com.ng>
2022-07-04 18:45:34 -0700

    4th fix on dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,11 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef FILE_DOG
+#define FILE_DOG
 
 /**
- * struct dog - creating a user-defined struct dog data type
- * @name: to rep name
- * @age: to rep the age
- * @owner: to rep owner
- * author: Sammykingx
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
  */
 
 struct dog
@@ -16,9 +15,14 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 void print_dog(struct dog *d);
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
+
 #endif
