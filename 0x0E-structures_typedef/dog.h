7064e6c8cf2b743cfd78803f7e319ec8ec4ce50a
sammykingx <hellosammy@sammykingx.com.ng>
2022-07-04 19:18:35 -0700

    testing1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,28 +1,21 @@
-#ifndef DOG
-#define DOG
-
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct dog - structure dog
- * @name: atribute pointer to char name's dog
- * @age: atribute float age's dog
- * @owner: atribute pointer to char owner's dog
- */
-
+ * struct dog - user defined data type
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
 
 #endif
