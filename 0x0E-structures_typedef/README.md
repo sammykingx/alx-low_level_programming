f51dd8d3ae781108970ee4ce0b0b1525c737f3a7
sammykingx <hellosammy@sammykingx.com.ng>
2022-07-04 18:24:03 -0700

    creating a new data type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+# About Directory
+The projects in this directory are part of the ALX SE Program curriculum which aim at exposing the students to user define data types. Here concepts like "struct" and "typedef" is essential as well as a prior knowledge of dynamic memory allocation in c programming.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - creating a user-defined struct dog data type
+ * @name: to rep name
+ * @age: to rep the age
+ * @owner: to rep owner
+ * author: Sammykingx
+ * Return: O if SUCCESSFUL
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
