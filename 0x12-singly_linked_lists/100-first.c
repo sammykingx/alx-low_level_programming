968f0c59e9fb22c8f5539f0ecdf02f8c0d0a5637
sammykingx <hellosammy@sammykingx.com.ng>
2022-07-14 19:49:20 -0700

    fixed check 04 error

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@
  */
 void __attribute__((constructor)) bmain()
 {
-    printf("You're beat! and yet, you must allow");
-    printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
