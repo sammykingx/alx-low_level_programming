a927e2f9366001be70b33eab1f3ec2c5c9463273
sammykingx <hellosammy@sammykingx.com.ng>
2022-07-14 18:50:14 -0700

    3rd for fixed task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,9 +13,9 @@ size_t print_list(const list_t *h)
 		return (1);
 
 	travnode = 0;
-	while (!(h))
+	while (h != NULL)
 	{
-		if(!(h->str))
+		if(h->str == NULL)
 			printf("[0] (nil)"\n);
 		else
 			printf("[%lu] %s\n", h->len, h->str);
