61be30fa38dc80c87954a1272d5eff927f733d63
sammykingx <hellosammy@sammykingx.com.ng>
2022-07-14 18:53:41 -0700

    4th for fixed task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if(h->str == NULL)
-			printf("[0] (nil)"\n);
+			printf("[0] (nil)\n");
 		else
 			printf("[%lu] %s\n", h->len, h->str);
 		h = h->next;
