9fc80da175b59201da8450bfba8d235a061aa8cf
sammykingx <hellosammy@sammykingx.com.ng>
2022-07-14 18:29:05 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * print_list - Function that prints the elements of a SLL
+ * @h: a pointer to the "HEAD" of the SLL
+ * Return: Returns size_t number of nodes
+ * Author: sammykingx
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodelen;
+	list_t *trevnode;
+
+	if (!(h))
+		return (1);
+
+	else if (!(h->str))
+		printf("[0] (nil)\n");
+
+	else
+		printf("[%lu] %s\n", h->len, h->str);
+
+	nodelen = 0;
+	trevnode = h;
+
+	while (!(trevnode))
+	{
+		trevnode = h->next;
+		nodelen++;
+	}
+
+	return (nodelen);
+}
