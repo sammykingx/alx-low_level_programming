719fb2a22ca51cfee6c9f8ea68fe175fca78f5af
sammykingx <hellosammy@sammykingx.com.ng>
2022-07-19 15:30:35 -0700

    function that returns the no of nodes in SLL

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 /**
  * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * @h: pointer to the 1st node of the linked list
  * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t nodenum = 0;
-	listint_t *temp = h;
 
 	while (temp)
 	{
 		nodenum++;
-		temp = temp->next;
+		h = h->next;
 	}
 	return (nodenum);
 }
