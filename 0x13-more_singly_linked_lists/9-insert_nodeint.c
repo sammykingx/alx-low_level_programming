d4b1716de4276f2028a3088ffbec45c12614d8bf
sammykingx <hellosammy@sammykingx.com.ng>
2022-07-20 17:38:34 -0700

    tried another approach (vg)

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,39 +13,39 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *new;
-	listint_t *h;
+	listint_t *node, *temp = *head;
+	size_t i = 0;
 
-	h = *head;
-
-	if (idx != 0)
-	{
-		for (i = 0; i < idx - 1 && h != NULL; i++)
-		{
-			h = h->next;
-		}
-	}
-
-	if (h == NULL && idx != 0)
+	node = malloc(sizeof(listint_t));
+	if (!node)
 		return (NULL);
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	node->n = n;
+	node->next = NULL;
+
+	if (!*head && !idx)
+	{
+		*head = node;
+		return (node);
+	}
+
+	else if (!*head && idx)
 		return (NULL);
 
-	new->n = n;
-
-	if (idx == 0)
+	else if (!idx)
 	{
-		new->next = *head;
-		*head = new;
-	}
-	else
-	{
-		new->next = h->next;
-		h->next = new;
+		node->next = temp;
+		*head = node;
+		return (node);
 	}
 
-	return (new);
+	while (i < (idx - 1))
+	{
+		temp = temp->next;
+		i++;
+	}
+	node->next = temp->next;
+	temp->next = node;
+
+	return (node);
 }
