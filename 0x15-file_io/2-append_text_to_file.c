8bb73786498e845d845ce74572c3ca3d25b1ffd5
sammykingx <hellosammy@sammykingx.com.ng>
2022-07-25 20:01:08 -0700

    task 02 {func that appends to a file}

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * append_text_to_file - This function adds text at EOF
+ * @filename: name of the file to append data to.
+ * @text_content: data to append to file
+ * Return: 1 if succesfull else -1 on err
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fild = 0, chk = 0, i = 0;
+
+	if (!filename)
+		return (-1);
+
+	fild = open(filename, O_WRONLY | O_APPEND);
+	if (fild < 0)
+		return (-1);
+
+	while (text_content[i])
+		i++;
+
+	chk = write(fild, text_content, i);
+	if (chk < 0)
+		return (-1);
+
+	close(fild);
+	return (1);
+}
