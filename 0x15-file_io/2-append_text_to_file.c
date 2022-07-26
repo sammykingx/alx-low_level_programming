dd8eee43584273aefb8dfdd1ba1d2ab28295bb00
sammykingx <hellosammy@sammykingx.com.ng>
2022-07-26 12:38:31 -0700

    appends text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,20 +9,22 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fild = 0, chk = 0, i = 0;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 
 	fild = open(filename, O_WRONLY | O_APPEND);
 	if (fild < 0)
 		return (-1);
 
-	while (text_content[i])
-		i++;
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
 
-	chk = write(fild, text_content, i);
-	if (chk < 0)
+		chk = write(fild, text_content, i);
+		if (chk < 0)
 		return (-1);
-
+	}
 	close(fild);
 	return (1);
 }
