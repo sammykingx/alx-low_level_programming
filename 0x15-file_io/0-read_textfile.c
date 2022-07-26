f8e3e022845f7bce62214d29bd9bb4b864120acd
sammykingx <hellosammy@sammykingx.com.ng>
2022-07-25 18:26:43 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * read_textfile - Reads a textfile and prints the contents to the POSIX STDOUT
+ * @filename: The name of the file to read from
+ * @letters: The number of characters it should print to the STDOUT
+ * Return: Returns the number of characters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fild = 0, chk = 0;
+	char *buffer;
+
+	if (!filename || !letters)
+		return (0);
+
+	fild = open(filename, O_RDONLY);
+	if (fild < 0)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
+
+	chk = read(fild, buffer, letters);
+	chk = write(STDOUT, buffer, chk);
+	if (chk < 0)
+		return (0);
+
+	close(fild);
+	free(buffer);
+	return (chk);
+}
